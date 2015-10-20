de62b5c859aee463a5c0d9033394bf863d469708
surberj <surberj@student.ethz.ch>
2015-10-20 17:32:45 +0200

    cleanup of variable names, orientation issue still not solved

--- postprocessing/main.m
+++ postprocessing/main.m
@@ -29,46 +29,55 @@ rovio = readtable(filename);
 % convert and time-align
 
 %% quaternion to euler convertion
+
 % ground_truth
-gt_q = [ground_truth.x_1, ground_truth.y_1, ground_truth.z_1, ground_truth.w];
-[gt_r1,gt_r2,gt_r3] = quat2angle(gt_q);
-
-% rovio
-ro_q = [rovio.x_1, rovio.y_1, rovio.z_1, rovio.w];
-[ro_r1,ro_r2,ro_r3] = quat2angle(ro_q);
-
-% % ground_truth
-gt_q = [ground_truth.x_1, ground_truth.y_1, ground_truth.z_1, ground_truth.w];
-R_vb = quat2dcm(gt_q(1,:)); % index v for vicon, b for body
+quat_vicon = [ground_truth.x_1, ground_truth.y_1, ground_truth.z_1, ground_truth.w];
 r_vicon = [ground_truth.x,ground_truth.y,ground_truth.z];
 
 % rovio
-ro_q = [rovio.x_1, rovio.y_1, rovio.z_1, rovio.w];
-R_rb = quat2dcm(ro_q(1,:)); % index r for rovio
+quat_rovio = [rovio.x_1, rovio.y_1, rovio.z_1, rovio.w];
 r_rovio = [rovio.x,rovio.y,rovio.z];
 
-% rovio wrt vicon
-R_vr = R_vb*R_rb';
+% time vectors
+time_vicon = ground_truth.rosbagTimestamp;
+time_rovio = rovio.rosbagTimestamp;
 
-% describe all signals wrt vicon frame
-% rovio: position:    r_rovio_in_vicon = R_vr * r_rovio
-%        orientation: ang_rovio_in_vicon = dcm3angle(R_vr * R_rb)
-% vicon: position:    r_vicon
-%        orientation: ang_vicon = dcm3angle(R_vb)
-
-r_rovio_in_vicon = zeros(length(r_rovio),3);
-ang_rovio_in_vicon = zeros(length(r_rovio),3);
-for i=1:length(r_rovio)
-    r_rovio_in_vicon(i,:) = R_vr*r_rovio(i,:)';
-    ang_rovio_in_vicon(i,:) = dcm2angle(R_vr*quat2dcm(ro_q(i,:)));
+% rotation matrix between rovio and vicon
+if time_vicon(1) < time_rovio(1)
+    ind_align_vicon = find(time_vicon >= time_rovio(1),1);
+    ind_align_rovio = 1;
+else
+    ind_align_vicon = 1;
+    ind_align_rovio = find(time_rovio >= time_vicon(1),1);
 end
 
-ang_vicon = zeros(length(r_vicon),3);
-[ang_vicon(:,1),ang_vicon(:,2),ang_vicon(:,3)] = quat2angle(gt_q);
+R_vb = quat2dcm(quat_vicon(ind_align_vicon,:));
+R_rb = quat2dcm(quat_rovio(ind_align_rovio,:));
+r_vb_in_v = r_vicon(ind_align_vicon,:)';
+r_rb_in_r = r_rovio(ind_align_rovio,:)';
+
+% constant rotation and translation between vicon and rovio frame
+R_vr = R_vb*R_rb';
+r_vr_in_v = r_vb_in_v - R_vr*r_rb_in_r;
+
+% describe all signals wrt vicon frame
+% rovio: position:    r_vb_in_vicon_rovio = r_vr_in_vicon + R_vr * r_rb_in_rovio
+%        orientation: ang_vb_in_vicon_rovio = dcm2angle(R_vr * R_rb)
+% vicon: position:    r_vb_in_vicon_vicon = r_vicon
+%        orientation: ang_vb_in_vicon_vicon = dcm3angle(R_vb)
+
+r_vb_in_vicon_rovio = zeros(length(r_rovio),3);
+ang_vb_in_vicon_rovio = zeros(length(r_rovio),3);
+for i=1:length(r_rovio)
+    r_vb_in_vicon_rovio(i,:) = r_vr_in_v + R_vr*r_rovio(i,:)';
+    ang_vb_in_vicon_rovio(i,:) = dcm2angle(R_vr*quat2dcm(quat_rovio(i,:)));
+end
+
+r_vb_in_vicon_vicon = r_vicon;
+ang_vb_in_vicon_vicon = zeros(length(r_vicon),3);
+[ang_vb_in_vicon_vicon(:,1),ang_vb_in_vicon_vicon(:,2),ang_vb_in_vicon_vicon(:,3)] = quat2angle(quat_vicon);
 
 %% plot
-time_gt = ground_truth.rosbagTimestamp;
-time_ro = rovio.rosbagTimestamp;
 %
 % init figure
 scrsz = get(groot,'ScreenSize');
@@ -76,9 +85,9 @@ figure('Name','Rovio analysis','NumberTitle','off', ...
     'Position',[1 1 scrsz(3) scrsz(4)]);
 % 
 ha(1) = subplot(321);
-plot(time_gt,r_vicon(:,1))
+plot(time_vicon,r_vb_in_vicon_vicon(:,1))
 hold on
-plot(time_ro,r_rovio_in_vicon(:,1))
+plot(time_rovio,r_vb_in_vicon_rovio(:,1))
 title('x tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)
@@ -87,9 +96,9 @@ ylabel('[m]','FontSize',12)
 grid on
 
 ha(2) = subplot(323);
-plot(time_gt,r_vicon(:,2))
+plot(time_vicon,r_vb_in_vicon_vicon(:,2))
 hold on
-plot(time_ro,r_rovio_in_vicon(:,2))
+plot(time_rovio,r_vb_in_vicon_rovio(:,2))
 title('y tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)
@@ -98,9 +107,9 @@ ylabel('[m]','FontSize',12)
 grid on
 
 ha(3) = subplot(325);
-plot(time_gt,r_vicon(:,3))
+plot(time_vicon,r_vb_in_vicon_vicon(:,3))
 hold on
-plot(time_ro,r_rovio_in_vicon(:,3))
+plot(time_rovio,r_vb_in_vicon_rovio(:,3))
 title('z tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)
@@ -109,9 +118,9 @@ ylabel('[m]','FontSize',12)
 grid on
 
 ha(4) = subplot(322);
-plot(time_gt,ang_vicon(:,1))
+plot(time_vicon,ang_vb_in_vicon_vicon(:,1))
 hold on
-plot(time_ro,ang_rovio_in_vicon(:,1))
+plot(time_rovio,ang_vb_in_vicon_rovio(:,1))
 title('euler r1 tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)
@@ -120,9 +129,9 @@ ylabel('[rad]','FontSize',12)
 grid on
 
 ha(4) = subplot(324);
-plot(time_gt,ang_vicon(:,2))
+plot(time_vicon,ang_vb_in_vicon_vicon(:,2))
 hold on
-plot(time_ro,ang_rovio_in_vicon(:,2))
+plot(time_rovio,ang_vb_in_vicon_rovio(:,2))
 title('euler r2 tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)
@@ -131,9 +140,9 @@ ylabel('[rad]','FontSize',12)
 grid on
 
 ha(4) = subplot(326);
-plot(time_gt,ang_vicon(:,3))
+plot(time_vicon,ang_vb_in_vicon_vicon(:,3))
 hold on
-plot(time_ro,ang_rovio_in_vicon(:,3))
+plot(time_rovio,ang_vb_in_vicon_rovio(:,3))
 title('euler r3 tracking','FontSize',12)
 h_legend = legend('ground truth', 'rovio');
 set(h_legend,'FontSize',8)

7d40c878bbb29a00ede5dd707aeb6295cdde0c04
surberj <surberj@student.ethz.ch>
2015-10-26 23:00:00 +0100

    mainly postprocessing improvements

--- postprocessing/scripts/bag_to_csv.py
+++ postprocessing/scripts/bag_to_csv.py
@@ -43,51 +43,90 @@
 import rosbag, sys, csv
 import time
 import string
-import shutil #for file management, copy filemport rosbag
+import os #for file management make directory
+import shutil #for file management, copy file
 
-#folder = '/home/julian/catkin_additions/log'
-#filename_bag = 'rovio_table_p22021.bag'
-#topic_name = ['/rovio/transform']
-
-if (len(sys.argv) == 3):
-	bag_file = sys.argv[1]
-	topic_name = sys.argv[2]	
-else:
+listOfBagFiles = []
+#verify correct input arguments: 1 or 2
+if (len(sys.argv) > 2):
 	print "invalid number of arguments:   " + str(len(sys.argv))
-	print "should be 3: 'bag_to_csv.py' and 'filename.bag' and 'topic_name'"
+	print "should be 2: 'bag2csv.py' and 'bagName'"
+	print "or just 1  : 'bag2csv.py'"
 	sys.exit(1)
-	
+elif (len(sys.argv) == 2):
+	f = sys.argv[1]
+	listOfBagFiles = [f]
+	numberOfFiles = str(1)
+	print "reading only 1 bagfile: " + str(listOfBagFiles[0])
+elif (len(sys.argv) == 1):
+	listOfBagFiles = [f for f in os.listdir(".") if f[-4:] == ".bag"]	#get list of only bag files in current dir.
+	numberOfFiles = str(len(listOfBagFiles))
+	print "reading all " + numberOfFiles + " bagfiles in current directory: \n"
+	for f in listOfBagFiles:
+		print f
+	print "\n press ctrl+c in the next 10 seconds to cancel \n"
+	time.sleep(10)
+else:
+	print "bad argument(s): " + str(sys.argv)	#shouldnt really come up
+	sys.exit(1)
 
-bag = rosbag.Bag(bag_file)
-bag_contents = bag.read_messages(topics=topic_name)
-bag_name = bag.filename
+count = 0
+for bagFile in listOfBagFiles:
+	count += 1
+	print "reading file " + str(count) + " of  " + numberOfFiles + ": " + bagFile
+	#access bag
+	bag = rosbag.Bag(bagFile)
+	bagContents = bag.read_messages()
+	bagName = bag.filename
+	print str(bagName)
 
-csv_file = string.rstrip(bag_file, ".bag") + '.csv'
-with open(csv_file,'w+') as csvfile:
-	filewriter = csv.writer(csvfile, delimiter = ',')
-	firstIteration = True	#allows header row
-	for subtopic, msg, t in bag_contents:
-		msgString = str(msg)
-		msgList = string.split(msgString, '\n')
-		instantaneousListOfData = []
-		for nameValuePair in msgList:
-			splitPair = string.split(nameValuePair, ':')
-			for i in range(len(splitPair)):	#should be 0 to 1
-				splitPair[i] = string.strip(splitPair[i])
-				if i==1:
+	#create a new directory
+	folder = string.rstrip(bagName, "bag")
+	folder = string.rstrip(folder,".")
+	print str(folder)
+	try:	#else already exists
+		os.makedirs(folder)
+	except:
+		pass
+
+
+	#get list of topics from the bag
+	listOfTopics = []
+	for topic, msg, t in bagContents:
+		if topic not in listOfTopics:
+			listOfTopics.append(topic)
+
+
+	for topicName in listOfTopics:
+		#Create a new CSV file for each topic
+		filename = folder + '/' + string.replace(topicName, '/', '_') + '.csv'
+		if filename.find("vicon") != -1:
+			filename = folder + '/' + 'vicon.csv'
+
+		print str(filename)
+		with open(filename, 'w+') as csvfile:
+			filewriter = csv.writer(csvfile, delimiter = ',')
+			
+
+			# assume topic contains estimated or ground truth pose --> write pose in the form (time tx ty tz qx qy qz qw)
+			for subtopic, msg, t in bag.read_messages(topicName):	# for each instant in time that has data for topicName
+				#parse data from this instant, which is of the form of multiple lines of "Name: value\n"
+				#	- put it in the form of a list of 2-element lists
+				msgString = str(msg)
+				msgList = string.split(msgString, '\n')
+				instantaneousListOfData = []
+				string_to_find = "xyzw"
+				for nameValuePair in msgList:
+					splitPair = string.split(nameValuePair, ':')
+					for i in range(len(splitPair)):	#should be 0 to 1
+						splitPair[i] = string.strip(splitPair[i])
 					instantaneousListOfData.append(splitPair)
-		#write the first row from the first element of each pair
-		if firstIteration:	# header
-			headers = ["rosbagTimestamp"]	#first column header
-			for pair in instantaneousListOfData:
-				headers.append(pair[0])
-			filewriter.writerow(headers)
-			firstIteration = False
-
-		# write the value from each pair to the file
-		values = [str(t)]	#first column will have rosbag timestamp
-		for pair in instantaneousListOfData:
-			values.append(pair[1])
-		filewriter.writerow(values)
-
-bag.close()
+				# write the value from each pair to the file
+				t = 0.000001* float(str(t))
+				values = [str(t)]	#first column will have rosbag timestamp [s]
+				for pair in instantaneousListOfData:
+					if string_to_find.find(pair[0]) != -1:
+						values.append(pair[1])
+				filewriter.writerow(values)
+	bag.close()
+print "Done reading all " + numberOfFiles + " bag files."
